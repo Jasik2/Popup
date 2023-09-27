154b7dfee734ed7a0d92b72b47840068549bb9d0
Jasik.Jakub <Jasik.Jakub@STACJA105-05>
2023-09-27 14:20:11 +0200

    Dodaj pliki projektów.

---
+++ Popup/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace Popup
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new mojpies();
+        }
+
+        private void BtnWyslij_Click(object sender, RoutedEventArgs e)
+        {
+            MyDog dog = DataContext as MyDog;
+            string dane = $"Imię właściciela: {txtImieWlasciciela.Text}\n" +
+                          $"Nazwisko właściciela: {txtNazwiskoWlasciciela.Text}\n" +
+                          $"Nazwa psa: {txtNazwaPsa.Text}\n" +
+                          $"Data urodzenia: {dog.DtB.ToShortDateString()}\n" +
+                          $"Rodowod: {txtNazwaPsa.Text}";
+            File.WriteAllText("dane.txt", dane);
+            MessageBox.Show("Dane zapisane!");
+        }
+    }
+
+
+
+    internal class MyDog : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        public void checkPropertyChangeEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+        private DateTime dtB;
+        public DateTime DtB
+        {
+            get { return dtB; }
+            set
+            {
+                dtB = value;
+                checkPropertyChangeEvent("DtB");
+            }
+        }
+    }
+}
---
+++ Popup/mojpies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+namespace Popup
+{
+    internal class mojpies
+    {
+        public MyDog Dog { get; set; } = new MyDog();
+
+        public mojpies()
+        {
+
+        }
+
+        public string ZbierzDane(string imie, string nazwisko, string nazwaPsa, DateTime dataUrodzenia)
+        {
+            return $"Imię właściciela: {imie}\n" +
+                   $"Nazwisko właściciela: {nazwisko}\n" +
+                   $"Nazwa psa: {nazwaPsa}\n" +
+                   $"Data urodzenia: {dataUrodzenia.ToShortDateString()}\n" +
+                   $"Rodowod: {nazwaPsa}\n";
+        }
+
+        public void ZapiszDane(string dane)
+        {
+            File.WriteAllText("dane.txt", dane);
+        }
+
+
+    }
+}
